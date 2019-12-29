8e4413386e8a615e9e49f739c3fcd9bf23aba226
Justin Beckwith <beckwith@google.com>
2019-12-29 10:57:58 -0800

    refactor: use explicit mocha imports

--- test/index.ts
+++ test/index.ts
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 import * as assert from 'assert';
+import {describe, it} from 'mocha';
 import * as stream from 'stream';
 import {replaceProjectIdToken} from '../src';
 
